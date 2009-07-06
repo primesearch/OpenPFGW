5fd231be484e20921d43ecb869584c11a325bf1e
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2009-07-06 13:39:23 +0000

    Call gwinit2() to verify gwnum version and compiler switches.  Remove commented (obsolete?) code.  Create new CheckForFatalError function that encapsulates all error checking after each square
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@59 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pform/pfgw/gw_prp.cpp
+++ pform/pfgw/gw_prp.cpp
@@ -12,6 +12,8 @@
 #endif
 
 extern int g_CompositeAthenticationLevel;
+extern const double maxErrorAllowed;
+bool CheckForFatalError(const char *caller, const char *sNumStr, GWInteger *gwX, int currentIteration, int maxIterations);
 
 // -2 is used for incomplete processing (i.e. user told us to exit early).
 // -1 is an error (round off or mod reduction).  It is NOT prime or composite.  We have NO idea what it is.
@@ -38,76 +40,6 @@ int gwPRP(Integer *N, const char *sNumStr, uint64 *p_n64ValidationResidue, uint3
 	--X;				// X is the exponent, we are to calculate 3^X mod N
 
 	int iTotal=lg(X);
-
-	// Could be a proth number.
-	/*
-
-	// Check for Proth.  We now have special code in GMP to handle them.  This changes
-	// the break over points SIGNIFICANTLY for when GMP is faster than Woltman logic.
-	bool bIsProth=false;
-	uint64 k;
-	uint32 n;
-	bool bPlus;
-	mpz_t *pI = N->gmp();
-	mp_limb_t t = mpz_getlimbn(*(N->gmp()), 0);
-
-	if (iTotal > 1200 && iTotal < 6000 && t == 1 || t == ((mp_limb_t)-1))
-	{
-		uint32 i = 1;
-		uint32 len = mpz_size(*pI);
-		if (t == 1)
-		{
-			for (; i < len; ++i)
-			{
-				if (mpz_getlimbn(*(N->gmp()), i))
-					break;
-			}
-			// Ok, this limb is not zero, so see if the bits fit a uint64
-			if (len-i <= 3)
-			{
-				if (len-2<=i)
-				{
-					// easiest cases (it always fits)
-					k = mpz_getlimbn(*(N->gmp()), len-1);
-					if (len-2==i)
-					{
-						k <<= 32;
-						k += mpz_getlimbn(*(N->gmp()), len-2);
-					}
-					n = 32*i;
-					while ( ((*(uint32*)&k) & 1) == 0)
-					{
-						++n;
-						k >>= 1;
-					}
-					bIsProth = bPlus = true;
-					X.Proth_prp(k, n, iBase);
-					if (X == 1)
-						return 1;
-					else if (p_n64ValidationResidue)
-						*p_n64ValidationResidue = (X & ((((uint64)1)<<62)-1));
-					return 0;
-				}
-				else
-				{
-					// uint32 sized limbs are ALL that will work in this code
-//					k = 0;
-//					uint32 lo = mpz_getlimbn(*(N->gmp()), i);
-//					// count bits of lo;
-//					uint32 lobits = 32;
-//					while ( (lo & 1) == 0)
-//					{
-//						lo >>= 1;
-//						--lobits;
-//					}
-				}
-			}
-		}
-		else
-		{
-		}
-	}
-	*/
 
 	// if iTotal is less than 1000, then use GMP to do exponentaion (we need to work out the exact cutoff's, and 
 	// different reduction methods will also "change" this".  However, this code (The current Winbloze build) is 
@@ -144,7 +76,23 @@ int gwPRP(Integer *N, const char *sNumStr, uint64 *p_n64ValidationResidue, uint3
 	}
 
 	// create a context
-	gwinit (&gwdata);
+   gwinit2(&gwdata, sizeof(gwhandle), GWNUM_VERSION);
+   if (gwdata.GWERROR == GWERROR_VERSION_MISMATCH)
+   {
+		PFOutput::EnableOneLineForceScreenOutput();
+		PFPrintfStderr ("GWNUM version mismatch.  PFGW is not linked with version %s of GWNUM.\n", GWNUM_VERSION);
+      g_bExitNow = true;
+      return 0;
+   }
+
+   if (gwdata.GWERROR == GWERROR_STRUCT_SIZE_MISMATCH)
+   {
+		PFOutput::EnableOneLineForceScreenOutput();
+		PFPrintfStderr ("GWNUM struct size mismatch.  PFGW must be compiled with same switches as GWNUM.\n");
+      g_bExitNow = true;
+      return 0;
+   }
+
 	gwsetmaxmulbyconst(&gwdata, iBase);	// maximum multiplier
 	if (CreateModulus(N)) return -2;
 
@@ -184,18 +132,6 @@ int prp_using_gwnum(Integer *N, uint32 iBase, const char *sNumStr, uint64 *p_n64
 	*RestoreName = 0;
 	if (iTotal > 50000)
 		CreateRestoreName(N, RestoreName);
-
-	// Handle "auto" round off checking.
-	extern bool g_bForceNoRoundOffChecking, g_bForceRoundOffChecking;
-	if (g_bForceNoRoundOffChecking)
-		ERRCHK=0;
-	else
-	{
-		if (!g_bForceRoundOffChecking)	// turn off error checking from last run if we are not "forced" on.
-			ERRCHK=0;
-		if (gwnear_fft_limit (&gwdata, 2.0))
-			ERRCHK=1;		// Getting close to max bits (within 2%)
-	}
 
 	int ThisLineLen_Final=0;
 	// everything with a GWInteger has a scope brace, so that
@@ -246,35 +182,13 @@ int prp_using_gwnum(Integer *N, uint32 iBase, const char *sNumStr, uint64 *p_n64
 		}
 
 		double MaxSeenDiff=0.;
-		for(;i--;) {
-			// These blocks of commented out items
-//			if (1 && (iDone >= 112240 || iDone == 25000 || iDone==50000))
-//			{
-//				char FN[40];
-//				sprintf(FN, "c:/pfgw_bit_%02d_pre_sqr", iDone+1);
-//				FILE *out = fopen(FN, "wt");
-//				for (int i = 0; i < FFTLEN; ++i)
-//				{
-//					fprintf(out, "%04d - %.2f\n", i, (gwX.G())[0]->data[i]);
-//				}
-//				fclose(out);
-//			}
+		for(;i--;)
+      {
+         int errchk = ErrorCheck(iDone, iTotal);
 
-			gw_clear_maxerr(&gwdata);
-			gwsetnormroutine(&gwdata,0,ERRCHK,bit(X,i));
+         gw_clear_maxerr(&gwdata);
+			gwsetnormroutine(&gwdata, 0, errchk, bit(X,i));
 			gwsquare(gwX);
-
-//			if (1 && (iDone >= 112240 || iDone == 25000 || iDone==50000))
-//			{
-//				char FN[40];
-//				sprintf(FN, "c:/pfgw_bit_%02d_post_sqr", iDone+1);
-//				FILE *out = fopen(FN, "wt");
-//				for (int i = 0; i < FFTLEN; ++i)
-//				{
-//					fprintf(out, "%04d - %.2f\n", i, (gwX.G())[0]->data[i]);
-//				}
-//				fclose(out);
-//			}
 
 			iDone++;
 			if(g_nIterationCnt && (((iDone%g_nIterationCnt)==0) || bFirst || !i))
@@ -285,7 +199,7 @@ int prp_using_gwnum(Integer *N, uint32 iBase, const char *sNumStr, uint64 *p_n64
 				bFirst=false;
 				// 120 bytes will not overflow, since we "force" the max size within the sprintf()
 				char Buf[160];
-				if (ERRCHK && gwdata.MAXDIFF != 1e80)
+				if (errchk)
 					sprintf(Buf, "PRP: %.36s %d/%d mro=%.5g sum=%.2f/%.2f\r",
                        sNumStr,iDone,iTotal, gw_get_maxerr(&gwdata), gwdata.MAXDIFF, MaxSeenDiff);
 				else
@@ -302,92 +216,16 @@ int prp_using_gwnum(Integer *N, uint32 iBase, const char *sNumStr, uint64 *p_n64
 				PFfflush(stderr);
 				ThisLineLen_Final = thisLineLen;
 			}
-			// Code "straight" from PRP.
-			if (gw_test_illegal_sumout (&gwdata)) {
-				PFOutput::EnableOneLineForceScreenOutput();
-				PFPrintfStderr("\n");
-				PFOutput::EnableOneLineForceScreenOutput();
-				PFOutput::OutputToErrorFileAlso("Detected in gw_test_illegal_sumout() in gwprp.cpp (PRP Testing)", sNumStr, iTotal-i, iTotal);
-				if (g_CompositeAthenticationLevel == 1)
-					PFPrintfStderr("Iteration: %d/%d ERROR: ILLEGAL SUMOUT\n   (Test aborted, try again using the -a2 (or possibly -a0) switch)\n", iTotal-i, iTotal);
-				else if (g_CompositeAthenticationLevel == 0)
-					PFPrintfStderr("Iteration: %d/%d ERROR: ILLEGAL SUMOUT\n   (Test aborted, try again using the -a1 switch)\n", iTotal-i, iTotal);
-				else
-					PFPrintfStderr("Iteration: %d/%d ERROR: ILLEGAL SUMOUT\n   (Test aborted)\n", iTotal-i, iTotal);
-				DestroyModulus();
-				return -1;
-			}
-			if (gw_test_mismatched_sums (&gwdata)) {
-//				static unsigned long last_bit = 0;
-//				static double last_suminp = 0.0;
-//				static double last_sumout = 0.0;
-				double suminp, sumout;
-				suminp = gwX.suminp ();
-				sumout = gwX.sumout ();
-//				if (bit == last_bit &&
-//					suminp == last_suminp &&
-//					sumout == last_sumout) {
-//					writeResults (ERROK);
-//					saving = 1;
-//				} else {
-//					char	msg[80];
-				PFOutput::EnableOneLineForceScreenOutput();
-				PFPrintfStderr("\n");
-				PFOutput::EnableOneLineForceScreenOutput();
-				PFOutput::OutputToErrorFileAlso("Detected in gw_test_mismatched_sums() in gwprp.cpp (PRP Testing)", sNumStr, iDone, iTotal);
-				if (g_CompositeAthenticationLevel == 1)
-					PFPrintfStderr("Iteration: %d/%d ERROR: SUM(INPUTS) != SUM(OUTPUTS),\n   %.16g != %.16g\n  (Diff=%.0f max allowed=%.0f)\n  (Test aborted, try again using the -a2 (or possibly -a0) switch)\n",  iTotal-i, iTotal, suminp, sumout, fabs(suminp-sumout), gwdata.MAXDIFF);
-				else if (g_CompositeAthenticationLevel == 0)
-					PFPrintfStderr("Iteration: %d/%d ERROR: SUM(INPUTS) != SUM(OUTPUTS),\n   %.16g != %.16g\n  (Diff=%.0f max allowed=%.0f)\n  (Test aborted, try again using the -a1 switch)\n",  iTotal-i, iTotal, suminp, sumout, fabs(suminp-sumout), gwdata.MAXDIFF);
-				else
-					PFPrintfStderr("Iteration: %d/%d ERROR: SUM(INPUTS) != SUM(OUTPUTS),\n   %.16g != %.16g\n  (Diff=%.0f max allowed=%.0f)\n  (Test aborted)\n",  iTotal-i, iTotal, suminp, sumout, fabs(suminp-sumout), gwdata.MAXDIFF);
-//				last_bit = bit;
-//				last_suminp = suminp;
-//				last_sumout = sumout;
-//				goto error;
-				DestroyModulus();
-				return -1;
-//				}
-			}
-//#define ROUND_OFF_CHECK_TEST
-			if (ERRCHK)
-			{
-				double d = gwX.sumdiff();
-				if ( (d-MaxSeenDiff) > 0.02)
-				{
-					MaxSeenDiff = d;
-#if defined (ROUND_OFF_CHECK_TEST)
-					// NOTE this is "special" testing code.  This is NOT for general release!!!
-					extern char g_szInputFileName[1024];
-					char FN[1060];
-					sprintf(FN, "%s_a%d_level.roundoff", g_szInputFileName, g_CompositeAthenticationLevel);
-					FILE *out = fopen(FN, "a");
-					if (out)
-					{
-						fprintf(out, "PRP: %.36s %d/%d mro=%.5g SM=%.2f/%.2f\n", sNumStr,iDone,iTotal, gw_get_maxerr(&gwdata), gwdata.MAXDIFF, MaxSeenDiff);
-						fclose(out);
-					}
-					else 
-						exit(printf ("Error opeing file %s\n", FN));
-#endif
-				}
-			}
-			if (gw_get_maxerr(&gwdata) > 0.40)
-			{
-				// Test for "excessive" error conditions
-				PFOutput::EnableOneLineForceScreenOutput();
-				PFPrintfStderr("\n");
-				PFOutput::EnableOneLineForceScreenOutput();
-				PFOutput::OutputToErrorFileAlso("Detected in MAXERR>0.4 (round off check) in gwprp.cpp (PRP Testing)", sNumStr, iDone, iTotal);
-				if (g_CompositeAthenticationLevel == 1)
-					PFPrintfStderr("Iteration: %d/%d ERROR: ROUND OFF %.5g>0.40\n   (Test aborted, try again using the -a2 (or possibly -a0) switch)\n",  iTotal-i, iTotal, gw_get_maxerr(&gwdata));
-				else if (g_CompositeAthenticationLevel == 0)
-					PFPrintfStderr("Iteration: %d/%d ERROR: ROUND OFF %.5g>0.40\n   (Test aborted, try again using the -a1 switch)\n",  iTotal-i, iTotal, gw_get_maxerr(&gwdata));
-				else
-					PFPrintfStderr("Iteration: %d/%d ERROR: ROUND OFF %.5g>0.40\n   (Test aborted)\n",  iTotal-i, iTotal, gw_get_maxerr(&gwdata));
-				DestroyModulus();
-				return -1;
-			}
+
+         if (errchk)
+	      {
+		      double d = gwX.sumdiff();
+		      if (d - MaxSeenDiff > 0.02)
+			      MaxSeenDiff = d;
+      	}
+
+         if (CheckForFatalError("prp_using_gwnum", sNumStr, &gwX, iDone, iTotal))
+            return -1;
 
 			if (g_bExitNow)
 			{
@@ -420,4 +258,62 @@ int prp_using_gwnum(Integer *N, uint32 iBase, const char *sNumStr, uint64 *p_n64
 		PFPrintfClearCurLine(ThisLineLen_Final);
 
 	return retval;
+}
+
+bool CheckForFatalError(const char *caller, const char *sNumStr, GWInteger *gwX, int currentIteration, int maxIterations)
+{
+   char  buffer1[200], buffer2[200], buffer3[200];
+   bool  haveFatalError = false;
+
+   // Code "straight" from PRP.
+	if (gw_test_illegal_sumout (&gwdata))
+   {
+      sprintf(buffer1, "Detected in gw_test_illegal_sumout() in %s", caller);
+      sprintf(buffer2, "Iteration: %d/%d ERROR: ILLEGAL SUMOUT", currentIteration, maxIterations);
+      buffer3[0] = 0;
+      haveFatalError = true;
+   }
+
+   if (!haveFatalError && gw_test_mismatched_sums (&gwdata))
+   {
+		double suminp, sumout;
+		suminp = gwX->suminp ();
+		sumout = gwX->sumout ();
+
+      sprintf(buffer1, "Detected in gw_test_mismatched_sums() in %s", caller);
+      sprintf(buffer2, "Iteration: %d/%d ERROR: SUM(INPUTS) != SUM(OUTPUTS),", currentIteration, maxIterations);
+      sprintf(buffer3, "%.16g != %.16g\n  (Diff=%.0f max allowed=%.0f)", suminp, sumout, fabs(suminp-sumout), gwdata.MAXDIFF);
+      haveFatalError = true;
+   }
+
+   if (gw_get_maxerr(&gwdata) > maxErrorAllowed)
+   {
+      sprintf(buffer1, "Detected in MAXERR>%.2f (round off check) in %s", maxErrorAllowed, caller);
+      sprintf(buffer2, "Iteration: %d/%d ERROR: ROUND OFF %.5g>%.2f", currentIteration, maxIterations, gw_get_maxerr(&gwdata), maxErrorAllowed);
+      buffer3[0] = 0;
+      haveFatalError = true;
+   }
+
+   if (haveFatalError)
+	{
+		PFOutput::EnableOneLineForceScreenOutput();
+		PFPrintfStderr("\n");
+		PFOutput::EnableOneLineForceScreenOutput();
+		PFOutput::OutputToErrorFileAlso(buffer1, sNumStr, currentIteration, maxIterations);
+
+      PFPrintfStderr("%s\n  ", buffer2);
+      if (buffer3[0] != 0)
+         PFPrintfStderr("%s\n  ", buffer3);
+
+      if (g_CompositeAthenticationLevel == 1)
+			PFPrintfStderr("(Test aborted, try again using the -a2 (or possibly -a0) switch)\n");
+		else if (g_CompositeAthenticationLevel == 0)
+			PFPrintfStderr("(Test aborted, try again using the -a1 switch)\n");
+		else
+			PFPrintfStderr("(Test aborted)\n");
+
+		DestroyModulus();
+	}
+
+   return haveFatalError;
 }
