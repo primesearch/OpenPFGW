ae2ba76f411f31c5d81162b3842f19d10a51a419
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2010-01-11 14:10:49 +0000

    Updated to address compiler warning
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@218 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pform/winpfgw/StdAfx.h
+++ pform/winpfgw/StdAfx.h
@@ -6,6 +6,8 @@
 #if !defined(AFX_STDAFX_H__1DC3A42B_B704_11D4_940F_00105AA797AB__INCLUDED_)
 #define AFX_STDAFX_H__1DC3A42B_B704_11D4_940F_00105AA797AB__INCLUDED_
 
+#include "targetver.h"
+
 #if _MSC_VER > 1000
 #pragma once
 #endif // _MSC_VER > 1000
