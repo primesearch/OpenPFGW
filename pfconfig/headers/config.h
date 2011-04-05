6eedf9c1c94b9ead1ac0303f7d5f1d1364a63633
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2011-04-05 13:11:49 +0000

    Removed unnecessary stuff
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@488 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pfconfig/headers/config.h
+++ pfconfig/headers/config.h
@@ -1,22 +1,3 @@
-/* config.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
-/*	PRIMEFORM/GW base configuration file						*/
-#define __OPENPFGW__
-
-/* Define as the return type of signal handlers (int or void).  */
-#define RETSIGTYPE void
-
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
-
-/* define whether or not to check for suboptimal adds   */
-/* #undef GW_ADDOPTIMIZE */
-
-/* define whether or not to check for unsafe multiplies   */
-/* #undef GW_MULCHECK */
-
-/* define whether or not to check for unsafe adds    */
-/* #undef GW_ADDCHECK */
 
 /* define whether to export the interface as inline functions */
 #define GW_INLINE inline
@@ -24,26 +5,8 @@
 /* define whether to export the interface as inline functions */
 #define GW_INLINE_ENABLED 1
 
-/* define whether or not to check for FFT vs non-FFT functions */
-/* #undef GW_FFTCHECK */
-
 /* define whether the compiler adds an underscore    */
 #define GW_UNDERSCORE
-
-/* Define if you have the strtol function.  */
-#define HAVE_STRTOL 1
-
-/* Define if you have the strtoul function.  */
-#define HAVE_STRTOUL 1
-
-/* Define if you have the <gmp.h> header file.  */
-#define HAVE_GMP_H 1
-
-/* Define if you have the <limits.h> header file.  */
-#define HAVE_LIMITS_H 1
-
-/* Define if you have the <sys/time.h> header file.  */
-#define HAVE_SYS_TIME_H 1
 
 #include "stdtypes.h"
 
