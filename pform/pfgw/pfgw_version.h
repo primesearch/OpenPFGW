e75e9ad7a472f8563f3644ad118fa34be93eed5e
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2011-09-22 18:36:27 +0000

    Updated build date
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@518 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pform/pfgw/pfgw_version.h
+++ pform/pfgw/pfgw_version.h
@@ -26,7 +26,7 @@
 //#define	RELEASE_LEVEL	2
 
 #define	RELEASE_VERSION         "3.5.0"
-#define	BUILD_DATE              "20110917"
+#define	BUILD_DATE              "20110922"
 
 #ifdef _64BIT
 #define  BITNESS                 "64BIT"
