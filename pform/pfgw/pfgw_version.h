312045a5f1623f978627bd9b38ffd04a84ef217a
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2011-09-23 13:25:31 +0000

    Updated for 3.5.1
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@525 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pform/pfgw/pfgw_version.h
+++ pform/pfgw/pfgw_version.h
@@ -25,8 +25,8 @@
 //#define	RELEASE_LEVEL	1
 //#define	RELEASE_LEVEL	2
 
-#define	RELEASE_VERSION         "3.5.0"
-#define	BUILD_DATE              "20110922"
+#define	RELEASE_VERSION         "3.5.1"
+#define	BUILD_DATE              "20110923"
 
 #ifdef _64BIT
 #define  BITNESS                 "64BIT"
