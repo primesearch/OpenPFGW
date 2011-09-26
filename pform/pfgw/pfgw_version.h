033e3943483fd34863c567913d1a3087c856006f
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2011-09-26 12:59:06 +0000

    Updated release and modified include path needed for projects
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@527 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pform/pfgw/pfgw_version.h
+++ pform/pfgw/pfgw_version.h
@@ -25,8 +25,8 @@
 //#define	RELEASE_LEVEL	1
 //#define	RELEASE_LEVEL	2
 
-#define	RELEASE_VERSION         "3.5.1"
-#define	BUILD_DATE              "20110923"
+#define	RELEASE_VERSION         "3.5.2"
+#define	BUILD_DATE              "20110926"
 
 #ifdef _64BIT
 #define  BITNESS                 "64BIT"
