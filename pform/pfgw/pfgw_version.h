d4bf020d033c989c504b1b9765a72ecb34b9617d
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2013-07-22 22:55:31 +0000

    Updated release number
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@612 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pform/pfgw/pfgw_version.h
+++ pform/pfgw/pfgw_version.h
@@ -25,8 +25,8 @@
 //#define	RELEASE_LEVEL	1
 //#define	RELEASE_LEVEL	2
 
-#define	RELEASE_VERSION         "3.7.6"
-#define	BUILD_DATE              "20130715"
+#define	RELEASE_VERSION         "3.7.7"
+#define	BUILD_DATE              "20130722"
 
 #ifdef _64BIT
 #define  BITNESS                 "64BIT"
