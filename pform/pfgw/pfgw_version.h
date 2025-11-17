f5dbf43a39a684cb29293625e2647469ce7c8596
mrodenkirch <mrodenkirch@368e28a5-49e5-40c5-be6c-d84989c25d7b>
2025-11-17 14:00:21 +0000

    Updated to gwnum 31.1
    git-svn-id: https://svn.code.sf.net/p/openpfgw/code@707 368e28a5-49e5-40c5-be6c-d84989c25d7b

--- pform/pfgw/pfgw_version.h
+++ pform/pfgw/pfgw_version.h
@@ -25,8 +25,8 @@
 //#define	RELEASE_LEVEL	1
 //#define	RELEASE_LEVEL	2
 
-#define	RELEASE_VERSION         "4.1.4"
-#define	BUILD_DATE              "20250415"
+#define	RELEASE_VERSION         "4.1.5"
+#define	BUILD_DATE              "20251117"
 
 #ifdef _64BIT
 #define  BITNESS                 "64BIT"
